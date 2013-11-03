5d5b1a8001a0616b4deeb4aac33af7c916d1d602
volanja <wandervogel.erde@gmail.com>
2013-11-03 20:56:40 +0900

    memcachedの設定ファイルをアップ(修正済み)

--- copy4git.sh
+++ copy4git.sh
@@ -2,4 +2,5 @@ sudo cp -r /usr/my.cnf usr/my.cnf
 sudo cp -r /etc/httpd/* etc/httpd/
 sudo cp -r /etc/nginx/* etc/nginx/
 sudo cp /etc/supervisord.conf etc/supervisord.conf
-
+sudo cp /etc/sysconfig/memcached etc/sysconfig/memcached
+sudo cp /etc/sysctl.conf etc/sysctl.conf
