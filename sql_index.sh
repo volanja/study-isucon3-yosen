fceaae5926a2650935a9927c54d29035aed49257
volanja <volaaanja@gmail.com>
2014-08-30 13:31:12 +0000

    Use --defaults-extra-file for mysql

--- sql_index.sh
+++ sql_index.sh
@@ -1,4 +1,6 @@
-mysql -u root -proot -e "use isucon;
+find /var/lib/mysql/isucon/ -type f |xargs -I % cat %  > /dev/null 
+
+mysql --defaults-extra-file=/home/isucon/webapp/my.cnf -e "use isucon;
 CREATE INDEX memos_idx_is_private_created_at ON memos (is_private,created_at); \
 CREATE INDEX memos_idx_user_created_at ON memos (user,created_at); \
 CREATE INDEX memos_idx_user_is_private_created_at ON memos (user,is_private,created_at); \
