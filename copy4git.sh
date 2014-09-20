1c11477b5e811be1a7b86aaa022eff07050525dd
volanja <volaaanja@gmail.com>
2014-09-20 05:56:06 +0000

    init Add Varnish

--- copy4git.sh
+++ copy4git.sh
@@ -1,6 +1,9 @@
 sudo cp -r /usr/my.cnf usr/my.cnf
 sudo cp -r /etc/httpd/* etc/httpd/
 sudo cp -r /etc/nginx/* etc/nginx/
+sudo cp -r /etc/varnish etc/
+
+sudo cp /etc/sysconfig/varnish etc/sysconfig/
 sudo cp /etc/supervisord.conf etc/supervisord.conf
 sudo cp /etc/sysconfig/memcached etc/sysconfig/memcached
 sudo cp /etc/sysctl.conf etc/sysctl.conf
--- copy4run.sh
+++ copy4run.sh
@@ -3,12 +3,14 @@ sudo service httpd stop
 sudo cp usr/my.cnf /usr/my.cnf
 #sudo cp -r /etc/httpd/* etc/httpd/
 sudo cp -r etc/nginx/* /etc/nginx/
+sudo cp -r etc/varnish/* /etc/varnish/
 sudo cp etc/supervisord.conf /etc/supervisord.conf
 sudo cp etc/sysconfig/memcached /etc/sysconfig/memcached
 sudo cp etc/sysctl.conf /etc/sysctl.conf
 sudo cp etc/init.d/memcached /etc/init.d/memcached
+sudo cp etc/sysconfig/varnish /etc/sysconfig/varnish
 
-
+sudo service varnish restart
 sudo service mysql restart
 sudo service nginx restart
 sudo service memcached restart
